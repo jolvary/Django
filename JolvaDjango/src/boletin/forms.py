2d1ba181f1df68924faf8b74fe7bd040c4574ace
Jolvary98 <jolvary@gmail.com>
2018-05-23 22:37:13 +0200

    Clase 16
    Model Form

--- JolvaDjango/src/boletin/forms.py
+++ JolvaDjango/src/boletin/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import Registrado
+
+class RegModelForm(forms.ModelForm):
+	class Meta:
+		model = Registrado
+		fields = ["nombre", "email"]
 
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
