1056ee6fb345f9ad7301f55e21bd93782ce00660
Jolvary98 <jolvary@gmail.com>
2018-05-24 21:58:02 +0200

    Clase 31
    Django Registration Redux 2

--- JolvaDjango/src/PruebaJolva/settings.py
+++ JolvaDjango/src/PruebaJolva/settings.py
@@ -141,4 +141,5 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_env", "media_root")
 
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+SITE_ID = 1
 CRISPY_TEMPLATE_PACK = "bootstrap3"
